0608f9bc8fce273674c510a120b963c7a3b468ea
Luis Pardo <luispardosuarez@gmail.com>
2023-10-23 12:01:08 +0200

    completed exercise!

--- src/ex/ex1/app.js
+++ src/ex/ex1/app.js
@@ -1,1 +1,15 @@
+/* Escribe un programa que escriba en la pantalla un texto de tipo <h1> que diga “Hello Javascript”. */
 
+// Crear una variable Hello Javasript
+// imprimir el dato en etiqueta h1 en el DOM
+
+const title = "Hello Javasript";
+const titleSection = document.getElementById('title');
+
+function printTitle(){
+    titleSection.innerHTML = /* html */ `
+    <h1>${title}</h1>
+    `
+}
+
+printTitle ()
